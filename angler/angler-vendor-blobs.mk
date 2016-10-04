b2c35be20bf8cfcef894dcdb2b83037abfaced47
beanstown106 <nbenis106@gmail.com>
2016-10-04 02:44:38 -0400

    angler: update proprietary blobs for 7.0.0 (NBD90X)
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- angler/angler-vendor-blobs.mk
+++ angler/angler-vendor-blobs.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 DirtyUnivorns
+# Copyright (C) 2016 The Pure Nexus Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,52 +13,22 @@
 # limitations under the License.
 
 PRODUCT_COPY_FILES += \
-    vendor/huawei/angler/proprietary/bin/fingerprintd:$(TARGET_COPY_OUT_SYSTEM)/bin/fingerprintd \
-    vendor/huawei/angler/proprietary/bin/mm-qcamera-app:$(TARGET_COPY_OUT_SYSTEM)/bin/mm-qcamera-app \
-    vendor/huawei/angler/proprietary/bin/nanoapp_cmd:$(TARGET_COPY_OUT_SYSTEM)/bin/nanoapp_cmd \
-    vendor/huawei/angler/proprietary/bin/ssr_setup:$(TARGET_COPY_OUT_SYSTEM)/bin/ssr_setup \
-    vendor/huawei/angler/proprietary/bin/subsystem_ramdump:$(TARGET_COPY_OUT_SYSTEM)/bin/subsystem_ramdump \
-    vendor/huawei/angler/proprietary/etc/diag/Angler_Radio-general.cfg:$(TARGET_COPY_OUT_SYSTEM)/etc/diag/Angler_Radio-general.cfg \
-    vendor/huawei/angler/proprietary/etc/permissions/cneapiclient.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/cneapiclient.xml \
-    vendor/huawei/angler/proprietary/etc/permissions/com.android.omadm.service.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.omadm.service.xml \
-    vendor/huawei/angler/proprietary/etc/permissions/com.google.widevine.software.drm.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.widevine.software.drm.xml \
-    vendor/huawei/angler/proprietary/etc/permissions/qcrilhook.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/qcrilhook.xml \
-    vendor/huawei/angler/proprietary/lib64/hw/activity_recognition.angler.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/activity_recognition.angler.so \
-    vendor/huawei/angler/proprietary/lib64/hw/audio.primary.msm8994.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/audio.primary.msm8994.so \
-    vendor/huawei/angler/proprietary/lib64/hw/context_hub.default.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/context_hub.default.so \
-    vendor/huawei/angler/proprietary/lib64/hw/gps.msm8994.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/gps.msm8994.so \
-    vendor/huawei/angler/proprietary/lib64/hw/nfc_nci.angler.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/nfc_nci.angler.so \
-    vendor/huawei/angler/proprietary/lib64/hw/sensors.angler.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/hw/sensors.angler.so \
-    vendor/huawei/angler/proprietary/lib64/libfilterpack_facedetect.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libfilterpack_facedetect.so \
-    vendor/huawei/angler/proprietary/lib64/libgps.utils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libgps.utils.so \
-    vendor/huawei/angler/proprietary/lib64/libimscamera_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libimscamera_jni.so \
-    vendor/huawei/angler/proprietary/lib64/libimsmedia_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libimsmedia_jni.so \
-    vendor/huawei/angler/proprietary/lib64/libhubconnection.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libhubconnection.so \
-    vendor/huawei/angler/proprietary/lib64/libloc_core.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libloc_core.so \
-    vendor/huawei/angler/proprietary/lib64/libloc_eng.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libloc_eng.so \
-    vendor/huawei/angler/proprietary/lib64/libManufacture.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libManufacture.so \
-    vendor/huawei/angler/proprietary/lib64/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libprotobuf-cpp-full.so \
-    vendor/huawei/angler/proprietary/lib64/libtinyxml.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libtinyxml.so \
-    vendor/huawei/angler/proprietary/lib/hw/activity_recognition.angler.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/activity_recognition.angler.so \
-    vendor/huawei/angler/proprietary/lib/hw/audio.primary.msm8994.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/audio.primary.msm8994.so \
-    vendor/huawei/angler/proprietary/lib/hw/camera.msm8994.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/camera.msm8994.so \
-    vendor/huawei/angler/proprietary/lib/hw/context_hub.default.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/context_hub.default.so \
-    vendor/huawei/angler/proprietary/lib/hw/gps.msm8994.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/gps.msm8994.so \
-    vendor/huawei/angler/proprietary/lib/hw/nfc_nci.angler.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/nfc_nci.angler.so \
-    vendor/huawei/angler/proprietary/lib/hw/sensors.angler.so:$(TARGET_COPY_OUT_SYSTEM)/lib/hw/sensors.angler.so \
-    vendor/huawei/angler/proprietary/lib/libdmengine.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libdmengine.so \
-    vendor/huawei/angler/proprietary/lib/libdmjavaplugin.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libdmjavaplugin.so \
-    vendor/huawei/angler/proprietary/lib/libfilterpack_facedetect.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libfilterpack_facedetect.so \
-    vendor/huawei/angler/proprietary/lib/libgps.utils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libgps.utils.so \
-    vendor/huawei/angler/proprietary/lib/libhubconnection.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libhubconnection.so \
-    vendor/huawei/angler/proprietary/lib/libimscamera_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libimscamera_jni.so \
-    vendor/huawei/angler/proprietary/lib/libimsmedia_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libimsmedia_jni.so \
-    vendor/huawei/angler/proprietary/lib/libloc_core.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libloc_core.so \
-    vendor/huawei/angler/proprietary/lib/libloc_eng.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libloc_eng.so \
-    vendor/huawei/angler/proprietary/lib/libmmcamera_interface.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmmcamera_interface.so \
-    vendor/huawei/angler/proprietary/lib/libmmjpeg_interface.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmmjpeg_interface.so \
-    vendor/huawei/angler/proprietary/lib/libmm-qcamera.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmm-qcamera.so \
-    vendor/huawei/angler/proprietary/lib/libqomx_core.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libqomx_core.so \
-    vendor/huawei/angler/proprietary/lib/libtinyxml.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libtinyxml.so \
-    vendor/huawei/angler/proprietary/lib/soundfx/libfmas.so:$(TARGET_COPY_OUT_SYSTEM)/lib/soundfx/libfmas.so \
-
+    vendor/huawei/angler/proprietary/bin/ssr_setup:system/bin/ssr_setup \
+    vendor/huawei/angler/proprietary/bin/subsystem_ramdump:system/bin/subsystem_ramdump \
+    vendor/huawei/angler/proprietary/etc/diag/Angler_Radio-general.cfg:system/etc/diag/Angler_Radio-general.cfg \
+    vendor/huawei/angler/proprietary/etc/permissions/cneapiclient.xml:system/etc/permissions/cneapiclient.xml \
+    vendor/huawei/angler/proprietary/etc/permissions/com.android.omadm.service.xml:system/etc/permissions/com.android.omadm.service.xml \
+    vendor/huawei/angler/proprietary/etc/permissions/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
+    vendor/huawei/angler/proprietary/lib/hw/audio.primary.msm8994.so:system/lib/hw/audio.primary.msm8994.so \
+    vendor/huawei/angler/proprietary/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
+    vendor/huawei/angler/proprietary/lib/libdmengine.so:system/lib/libdmengine.so \
+    vendor/huawei/angler/proprietary/lib/libdmjavaplugin.so:system/lib/libdmjavaplugin.so \
+    vendor/huawei/angler/proprietary/lib/libimscamera_jni.so:system/lib/libimscamera_jni.so \
+    vendor/huawei/angler/proprietary/lib/libimsmedia_jni.so:system/lib/libimsmedia_jni.so \
+    vendor/huawei/angler/proprietary/lib/libmm-qcamera.so:system/lib/libmm-qcamera.so \
+    vendor/huawei/angler/proprietary/lib/libtinyxml.so:system/lib/libtinyxml.so \
+    vendor/huawei/angler/proprietary/lib64/hw/audio.primary.msm8994.so:system/lib64/hw/audio.primary.msm8994.so \
+    vendor/huawei/angler/proprietary/lib64/libimscamera_jni.so:system/lib64/libimscamera_jni.so \
+    vendor/huawei/angler/proprietary/lib64/libimsmedia_jni.so:system/lib64/libimsmedia_jni.so \
+    vendor/huawei/angler/proprietary/lib64/libManufacture.so:system/lib64/libManufacture.so \
+    vendor/huawei/angler/proprietary/lib64/libtinyxml.so:system/lib64/libtinyxml.so
--- angler/device-vendor.mk
+++ angler/device-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 DirtyUnivorns
+# Copyright (C) 2016 The Pure Nexus Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,58 +14,37 @@
 
 $(call inherit-product, vendor/huawei/angler/angler-vendor-blobs.mk)
 
-# Prebuilt APKs/JARs from 'vendor/app'
-PRODUCT_PACKAGES += \
-    TimeService \
-    ims
-
-# Prebuilt APKs libs symlinks from 'vendor/app'
-PRODUCT_PACKAGES += \
-    libimscamera_jni_64.so \
-    libimsmedia_jni_64.so
-
-# Prebuilt APKs/JARs from 'proprietary/app'
+# Prebuilt APKs
 PRODUCT_PACKAGES += \
     HwMMITest \
-    Tycho \
     HwSarControlService \
-    SetupSmartDeviceOverlay
+    ims \
+    SetupSmartDeviceOverlay \
+    TimeService \
+    Tycho 
 
-# Prebuilt APKs libs symlinks from 'proprietary/app'
+# Prebuilt jars
 PRODUCT_PACKAGES += \
-    libManufacture_64.so
+    qcrilhook
 
-# Prebuilt APKs/JARs from 'proprietary/framework'
+# Prebuilt privileged APKs
 PRODUCT_PACKAGES += \
-    qcrilhook \
-    com.google.widevine.software.drm \
-    com.google.android.camera.experimental2015
-
-# Prebuilt APKs/JARs from 'proprietary/priv-app'
-PRODUCT_PACKAGES += \
-    HotwordEnrollment \
-    Entitlement \
-    DCMO \
-    SprintDM \
+    CallStatistics \
     CNEService \
     ConnMO \
-    DMService \
-    CallStatistics \
+    DCMO \
     DiagMon \
-    qcrilmsgtunnel \
-    CarrierEntitlement \
+    DMService \
+    GCS \
     HiddenMenu \
-    ConfigUpdater
+    HotwordEnrollment \
+    qcrilmsgtunnel \
+    SprintDM
 
-# Prebuilt APKs libs symlinks from 'proprietary/priv-app'
+# Symlinks
 PRODUCT_PACKAGES += \
-    libdmengine_32.so \
-    libdmjavaplugin_32.so
-
-# Prebuilt shared libraries from 'vendor'
-PRODUCT_PACKAGES += \
-    libqmi_common_so \
-    libqmi_csi \
-    libdsi_netctrl \
-    libqmiservices \
-    libqmi_cci
+    libimsmedia_jni.so \
+    libimscamera_jni.so \
+    libdmengine.so \
+    libdmjavaplugin.so \
+    libManufacture.so
